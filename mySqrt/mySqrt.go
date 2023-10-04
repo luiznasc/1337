49f9a89478d46b611feaeb0c07a88e02b48f4f76
luiznasc <luiz.nascimen@ccc.ufcg.edu.br>
2023-10-04 04:03:13 -0300

    can be further optimized on the increment

--- mySqrt/mySqrt.go
+++ mySqrt/mySqrt.go
@@ -1,13 +1,23 @@
 package main
 
 func mySqrt(x int) int {
-	count := 0
+	count := x
+	upperEdge := x
 	for {
 		if count*count > x {
-			count--
-			break
+			upperEdge = count
+			count = count / 2
+		} else {
+			if count*count == x {
+				return count
+			}
+			for i := count; i <= upperEdge; i++ {
+				if i*i > x {
+					return i - 1
+				} else if i*i == x {
+					return i
+				}
+			}
 		}
-		count++
 	}
-	return count
 }
